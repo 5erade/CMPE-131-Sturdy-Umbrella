102ba3c9ed4eac1a550a2912c3ce1ae5be063a86
5erade <aaron.rodda@sjsu.edu>
2021-12-03 12:33:11 -0800

    Added:  -ability to change player Symbols  -reset button clears array & board  -condition for Tie game (cat's game) Removed:  -refresh page button

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
-import {Button, Typography} from "@mui/material";
+import {Button, Grid, TextField, Typography} from "@mui/material";
 import styled from "@emotion/styled";
 import './Game.css';
+import {Box} from "@material-ui/core";
+import AutorenewIcon from "@mui/icons-material/Autorenew";
 const MyButton = styled(Button)({
     border: '2px solid',
     borderRadius: 0,
@@ -28,21 +30,45 @@ class Tile extends React.Component {
     }
 }
 
+
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             gameTiles: Array(9).fill(null),
             nextMove: true,
+            symbolX: 'X',
+            symbolO:'O'
         };
+
+        this.changeSymbolX = this.changeSymbolX.bind(this);
+        this.changeSymbolO = this.changeSymbolO.bind(this);
+
+    }
+    resetBoard= (event) => {
+        this.setState({gameTiles: event.target.fill(null)});
+    }
+    changeSymbolX= (event) => {
+        this.setState({symbolX: event.target.value});
     }
 
+    changeSymbolO= (event) => {
+        this.setState({symbolO: event.target.value});
+    }
+    handleClickReset(){
+        const tiles = this.state.gameTiles.fill(null);
+
+        this.setState({
+            gameTiles: tiles,
+            nextMove: this.state.symbolX
+        })
+    }
     handleClick(i) {
         const tiles = this.state.gameTiles.slice();
         if (calculateWinner(tiles) || tiles[i]) {
             return;
         }
-        tiles[i] = this.state.nextMove ? 'X' : 'O';
+        tiles[i] = this.state.nextMove ? this.state.symbolX  : this.state.symbolO ;
 
         this.setState({
             gameTiles: tiles,
@@ -52,28 +78,44 @@ class Board extends React.Component {
 
     renderTile(i) {
         return (
-
                 <Tile
                     value={this.state.gameTiles[i]}
                     onClick={() => this.handleClick(i)}
                 />
-
-
-
         );
     }
-
     render() {
         const winner = calculateWinner(this.state.gameTiles);
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.nextMove ? 'X' : 'O');
+        } else if(winner === 0){
+            status = 'Cat\'s Game';
+        }
+        else {
+            status = 'Next player: ' + (this.state.nextMove ? this.state.symbolX  : this.state.symbolO );
         }
 
         return (
             <div>
+                <TextField
+                    sx={{borderColor:'#ffffff'}}
+                    //defaultValue={'X'}
+                    label="Player 1 Icon"
+                    variant="outlined"
+                    value={this.state.symbolX}
+                    onChange={this.changeSymbolX}
+                />
+                <br/>
+                <br/>
+                <TextField
+                    sx={{borderColor:'#ffffff'}}
+                    //defaultValue={'O'}
+                    label="Player 2 Icon"
+                    variant="outlined"
+                    value={this.state.symbolO}
+                    onChange={this.changeSymbolO}
+                />
                 <Typography variant={"h4"}>
                     <div className="status">
                         {status}
@@ -88,18 +130,41 @@ class Board extends React.Component {
                 <div className="board-row">
                     {this.renderTile(6)}{this.renderTile(7)}{this.renderTile(8)}
                 </div>
+                <div>
+                    <Button sx={{ m:1, borderColor:'#ffffff'}} style={{
+                        color: '#ffffff',
+                    }} variant={"outlined"}    onClick={() => this.handleClickReset()
+                    }>
+                        Reset &nbsp;
+                        <AutorenewIcon/>
+                    </Button>
+                </div>
+                <br/>
+
             </div>
         );
     }
 }
 
+
+
+
 class Game extends React.Component {
     render() {
         return (
             <div className="game">
+
+            <Grid>
+                    <Box m={5}>
+
+                    </Box>
+            </Grid>
+            <Grid>
                 <div className="game-board">
                     <Board />
                 </div>
+            </Grid>
+
             </div>
         );
     }
@@ -111,13 +176,20 @@ function calculateWinner(gameTiles) {
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6],
     ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (gameTiles[a] && gameTiles[a] === gameTiles[b] && gameTiles[a] === gameTiles[c]) {
-            return gameTiles[a];
+
+        if (gameTiles.includes(null) === true) {
+            for (let i = 0; i < lines.length; i++) {
+                const [a, b, c] = lines[i];
+                if (gameTiles[a] && gameTiles[a] === gameTiles[b] && gameTiles[a] === gameTiles[c]) {
+                    return gameTiles[a];
+                }
+            }
+            return null;
+        } else{
+            return 0;
         }
-    }
-    return null;
+
+
 }
 
 export default Game;
--- src/GamePage.js
+++ src/GamePage.js
@@ -1,30 +1,20 @@
 import React, {Component} from 'react';
 import Game from "./Game";
-import {Box,  Grid} from "@material-ui/core";
-import {Button, Typography} from "@mui/material";
-import AutorenewIcon from '@mui/icons-material/Autorenew';
-import TextField from '@mui/material/TextField';
+import { Grid} from "@material-ui/core";
+import { Typography} from "@mui/material";
 
 
-function refreshPage() {
-    window.location.reload(false);
-}
+export class GamePage extends Component {
 
-class GamePage extends Component {
     render() {
         return (
             <div   style={{
-                backgroundColor: 'grey',
+                backgroundColor: '#4d4d4d',
 
             }}>
                 <Grid container spacing={1}>
                     <Grid item xs={4}>
-                        <Box m={20}>
-                            <TextField sx={{ m: 1, borderColor:'#ffffff'}} ref="p1" defaultValue={'X'} id="player1" label="Player 1 Icon" variant="outlined" >0</TextField>
-                            <br/>
-                            <TextField sx={{ m: 1, color:'#ffffff'}} ref="p2" defaultValue={'O'} id="player2" label="Player 2 Icon" variant="outlined" />
-                            <br/>
-                        </Box>
+
                     </Grid>
                     <Grid item xs={4}>
                         <Typography  align={"center"}>
@@ -36,17 +26,6 @@ class GamePage extends Component {
                             </Typography>
                             <br/>
                             <Game/>
-                            <Button sx={{ m:1, borderColor:'#ffffff'}} style={{
-                                color: '#ffffff',
-                            }} variant={"outlined"} onClick={refreshPage
-
-                            }>
-                                Reset &nbsp;
-
-                                <AutorenewIcon/>
-
-                            </Button>
-
 
                         </Typography>
                         <Grid item xs={4}>
